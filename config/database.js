4e1966c600ed4bf33ea35bae122ef9f57f30b0ba
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-10 11:04:42 +0700

    edited configuration for mysql cloud

--- config/database.js
+++ config/database.js
@@ -6,11 +6,24 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: 3306,
-    logging: false, // Matikan log SQL agar console bersih
-    timezone: '+07:00' // Wajib set timezone Indonesia
+    host: process.env.DB_HOST.trim(),
+    dialect: 'mysql',
+    port: parseInt(process.env.DB_PORT) || 15462,
+    logging: false,
+    timezone: '+07:00',
+    dialectOptions: {
+      connectTimeout: 60000, // Perpanjang timeout jadi 60 detik
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
+    }
   }
 );
 
