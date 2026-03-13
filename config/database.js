e473065752c726f9d35b1e7aab2022bca6455df4
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-13 14:49:34 +0700

    dialect revision

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST.trim(),
-    dialect: 'mysql2',
+    dialect: 'mysql',
     port: parseInt(process.env.DB_PORT) || 15462,
     logging: false,
     timezone: '+07:00',
